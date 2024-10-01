8ea79692e1436628481620af96100999e980dd7f
FredTheNoob <frederikhammer02@gmail.com>
2024-10-01 10:56:53 +0200

    Whisper Cuda installation docker

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,22 @@
-FROM python:3
+# Use the python:3.10.12 base image
+FROM python:3.10.12
 
+# Argument to set the environment (development/production)
+ARG ENV
+
+# Set the working directory inside the container
 WORKDIR /app/
 
+# Copy all files to the working directory
 COPY . .
 
-ENTRYPOINT [ "python3", "./src/" ]
+RUN mkdir -p /usr/local/cuda
+
+# Set environment variable based on the ARG
+ENV mode=$ENV
+
+# Verbose logging to ensure each step is tracked
+ENTRYPOINT [ "sh", "./prod.sh" ]
+
+# Entry point to run the Python application
+# CMD [ "python3", "./src/" ]
--- src/__main__.py
+++ src/__main__.py
@@ -1,1 +1,24 @@
-print("Hello from backend")
+#import os
+
+#print("Hello from backend: %s" % os.environ["DEVICE"])
+
+print("Hello from backend") 
+
+from faster_whisper import WhisperModel
+
+model_size = "tiny"
+
+# Run on GPU with FP16
+#model = WhisperModel(model_size, device="cuda", compute_type="float16")
+
+# or run on GPU with INT8
+# model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
+# or run on CPU with INT8
+model = WhisperModel(model_size, device="auto", compute_type="int8")
+
+segments, info = model.transcribe("/app/src/rick.wav", beam_size=5)
+
+print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
+
+for segment in segments:
+    print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
