4ad867523df63f0d5752d504845235e7803a31b7
FredTheNoob <frederikhammer02@gmail.com>
2024-10-03 16:09:40 +0200

    fixed

--- prod.sh
+++ prod.sh
@@ -3,6 +3,7 @@
 if [ "$ENV" = "production" ]; then
   echo "Running in production mode..."
   pip3 install nvidia-cudnn-cu12==8.9.5.30
+  export LD_LIBRARY_PATH=`python3 -c 'import os; import nvidia.cublas.lib; import nvidia.cudnn.lib; print(os.path.dirname(nvidia.cublas.lib.__file__) + ":" + os.path.dirname(nvidia.cudnn.lib.__file__))'`
 else
   echo "Running in development mode..."
   # Any development-related setup or commands
