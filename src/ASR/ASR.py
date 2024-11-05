1d86eb9891e7dd004e208026186574a34e9a94c5
FredTheNoob <frederikhammer02@gmail.com>
2024-11-05 08:26:32 +0100

    try make shit work, no luck

--- src/ASR/ASR.py
+++ src/ASR/ASR.py
@@ -1,21 +1,36 @@
+from asyncio import sleep
 from io import BytesIO
 import re
 from typing import List
+import wave
 from ASR.LocalAgreement import LocalAgreement
 from faster_whisper import WhisperModel 
 import soundfile as sf
+from pydub import AudioSegment
 
 class ASR:
     max_context_length = 100
-    audio_buffer: BytesIO = BytesIO()
+    metadata: BytesIO = BytesIO()
+    audio_buffer: List[BytesIO] = []
     local_agreement = LocalAgreement()
     context:str = ""
     confirmed_sentences: List[str] = []
+    min_chunk_size = 3
+
     def __init__ (self, model_size: str, device="auto", compute_type = "int8", max_context_length=100):
         self.whisper_model = WhisperModel(model_size, device=device, compute_type=compute_type)
         self.max_context_length = max_context_length
         
     def transcribe(self, audio_buffer: BytesIO, context: str):
+        print(audio_buffer.getbuffer().nbytes)
+
+        audio_buffer.seek(0)
+        # a lil debug tang
+        with open("temp.webm", 'wb') as f:
+            f.write(audio_buffer.getvalue())
+
+        audio_buffer.seek(0)
+        
         transcribed_text = ""
         segments, info = self.whisper_model.transcribe(audio_buffer, beam_size=5, initial_prompt=context)
         
@@ -24,24 +39,37 @@ class ASR:
             
         return transcribed_text
     
-    def process_audio(self, audio_chunk) -> str:
-        # Append new audio data to the main buffer
-        self.audio_buffer.write(audio_chunk)
-        self.audio_buffer.seek(0)  # Reset buffer's position to the beginning
+    def save_metadata(self, metadata):
+        print("saving metadata")
+        self.metadata.write(metadata)
+        self.metadata.seek(0)  # Reset buffer's position to the beginning
 
-        #print(self.audio_buffer.getbuffer().nbytes)
+    def recieve_audio_chunk(self, audio_chunk):
+        print("recieving audio chunk")
+        self.audio_buffer.append(BytesIO(audio_chunk))
+
+        print("audio buffer length: ", len(self.audio_buffer))
+        print("min chunk size: ", self.min_chunk_size)
+        if len(self.audio_buffer) > self.min_chunk_size:
+            self.process_audio()
+    
+    def process_audio(self) -> str:
+        print("processing audio")
+        # transcribed_text = self.transcribe(BytesIO(self.metadata.read() + self.audio_buffer.read()), self.context)
+        combined_bytes = self.metadata.getvalue() + b''.join(bio.getvalue() for bio in self.audio_buffer)
+        combined_bytes_io = BytesIO(combined_bytes)
         
-        transcribed_text = self.transcribe(self.audio_buffer, self.context)
-        #print("transcribed_text: " + transcribed_text)
+        transcribed_text = self.transcribe(combined_bytes_io, self.context)
+        print(transcribed_text)
         confirmed_text = self.local_agreement.confirm_tokens(transcribed_text)
-        print(confirmed_text)
+        #print(confirmed_text)
         punctuation = r"[.!?]"  # Regular expression pattern for ., !, or ?
         # Detect punctuation
 
-        if len(self.context) > self.max_context_length:
-            self.confirmed_sentences = self.confirmed_sentences[-2:]
-            self.context = " ".join(self.confirmed_sentences)
-            #print("context truncated: " + self.context)
+        # if len(self.context) > self.max_context_length:
+        #     self.confirmed_sentences = self.confirmed_sentences[-2:]
+        #     self.context = " ".join(self.confirmed_sentences)
+        #     #print("context truncated: " + self.context)
 
         #print("check punctuation: ", re.search(punctuation,confirmed_text))
         if re.search(punctuation,confirmed_text):
@@ -52,11 +80,11 @@ class ASR:
 
             #print("sentence", sentence)
             self.confirmed_sentences.append(sentence[-1])
-            self.context = " ".join(self.confirmed_sentences)
-            #print("context added: " + self.context)
+            # self.context = " ".join(self.confirmed_sentences)
+            # print("context added: " + self.context)
             
             self.local_agreement.clear_confirmed_text()
             
-            self.audio_buffer = BytesIO()
+            self.audio_buffer = []
             
         return confirmed_text
--- src/__main__.py
+++ src/__main__.py
@@ -6,13 +6,22 @@ from ASR.ASR import ASR
 
 _ASR = ASR("tiny", "auto","int8", 150)
 
+
 async def handler(websocket):
+    has_received_first_byte = False
+
     while True:
         try:
             # Receiving binary data directly from the client
             data = await websocket.recv()
+
+            if has_received_first_byte:
             #Handle the audion data with Whisper
-            _ASR.process_audio(data)
+                _ASR.recieve_audio_chunk(data)
+            else: 
+                has_received_first_byte = True
+                _ASR.save_metadata(data)
+
             # Optionally, send an acknowledgment back to the client
             await websocket.send("Chunk received")
         except websockets.ConnectionClosed:
