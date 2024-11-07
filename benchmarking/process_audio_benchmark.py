89c2506202f01bedf2a18bcafec8fecfd679eb66
Aasmundur <aakt21@student.aau.dk>
2024-11-07 08:41:19 +0100

    The WER benchmark and latency for process_audio are here, together with some testing soundfiles and txt

--- benchmarking/process_audio_benchmark.py
+++ benchmarking/process_audio_benchmark.py
@@ -1,19 +1,29 @@
+import time
+
 import librosa
 import numpy as np
 import jiwer
 from src.ASR.ASR import ASR
-async def process_audio_benchmark(wav_filename, txt_filename):
-    print(f"HERE {wav_filename}!!!!!")
+async def process_audio_benchmark(wav_filename, txt_filename, asr):
     try:
         print(f"Processing {wav_filename} and {txt_filename}")
         transcribed_text = ""
         actual_text = ""
-        asr = ASR(model_size="base", device="auto", compute_type="float32")
         audio, _ = librosa.load(wav_filename, sr=16000, dtype=np.float32)
         # Chunk the audio into 2 second chunks
         chunk_size = 16000 * 2
+
+        #Start timer
+        start_total_time = time.time()
+        rounds = 0.0
         for i in range(0, len(audio), chunk_size):
             transcribed_text += asr.process_audio(audio[i:i + chunk_size])
+            rounds += 1
+        #End timer
+        end_total_time = time.time()
+        #total time elapsed
+        total_time = end_total_time - start_total_time
+        average_time_per_chunk = total_time / rounds
         with open(txt_filename, "r") as f:
             actual_text = f.read()
         transforms = jiwer.Compose(
@@ -28,6 +38,8 @@ async def process_audio_benchmark(wav_filename, txt_filename):
             ]
         )
         wer = jiwer.wer(actual_text, transcribed_text, truth_transform=transforms, hypothesis_transform=transforms)
-        print(f"WER: {wer} between {txt_filename} and {wav_filename}")
+        print(f"    WER: {wer} between {txt_filename} and {wav_filename}")
+        print(f"    Total time using process_audio on {wav_filename}: {total_time} seconds")
+        print(f"    Average time pr. chunk: {average_time_per_chunk} seconds, chunk size: {chunk_size}")
     except Exception as e:
         print(f"Error processing {wav_filename} and {txt_filename}: {e}")
--- benchmarking/run_benchmarks.py
+++ benchmarking/run_benchmarks.py
@@ -1,10 +1,13 @@
 import asyncio
 
 from benchmarking.process_audio_benchmark import process_audio_benchmark
+from src.ASR.ASR import ASR
+
 
 async def run_benchmarks():
-    await process_audio_benchmark("testfiles/MaleList57.wav", "testfiles/Male.txt")
-    await process_audio_benchmark("testfiles/femaleList3.wav", "testfiles/Female.txt")
+    asr = ASR(model_size="base", device="auto", compute_type="float32")
+    await process_audio_benchmark("testfiles/MaleList57.wav", "testfiles/Male.txt", asr)
+    await process_audio_benchmark("testfiles/femaleList3.wav", "testfiles/Female.txt", asr)
 
 
 asyncio.run(run_benchmarks())
