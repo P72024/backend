910e169bc95d08fa1645e7c637771a4cdf75ec31
FredTheNoob <frederikhammer02@gmail.com>
2024-11-11 11:03:12 +0100

    recieve client id from websocket data

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,6 @@
 import asyncio
 from io import BytesIO
+import json
 
 import websockets
 from ASR.ASR import ASR
@@ -18,14 +19,23 @@ async def handler(websocket):
         async for message in websocket:
             transcribed_text = ''
 
+            data = json.loads(message)
+
+            client_id = data.get('clientId')
+            audio_data = data.get('audioData')
+            audio_data = bytes(audio_data)
+
+            print("client_id: ", client_id)
+
             if has_received_first_byte:
-                transcribed_text = _ASR.receive_audio_chunk(message)
+                transcribed_text = _ASR.receive_audio_chunk(audio_data)
             else:
-                _ASR.save_metadata(message)
+                _ASR.save_metadata(audio_data)
                 has_received_first_byte = True
 
             if transcribed_text != '':
-                await websocket.send("transcribed_text: " + transcribed_text)
+                print("sending to client: ", client_id)
+                await websocket.send(transcribed_text)
     except websockets.ConnectionClosed:
         print(f"Client disconnected")
     finally:
