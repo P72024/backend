96d9313e68c85701f43169ca246018dc50eec4d6
Ucloud server <ucloud@130.225.38.116>
2024-11-18 09:28:56 +0100

    from server

--- benchmarking/process_audio_benchmark.py
+++ benchmarking/process_audio_benchmark.py
@@ -5,11 +5,8 @@ import time
 from io import BytesIO
 
 import jiwer
-import librosa
 import numpy as np
-from aiortc.contrib.media import MediaPlayer
 from prettytable import PrettyTable
-from pydub import AudioSegment
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 from ASR.ASR import ASR
@@ -44,12 +41,6 @@ async def process_audio_benchmark(chunks_pkl, meta_pkl, txt_filename, size, max_
         max_chunk_time = "{:.7f}".format(max(times))
         min_chunk_time = "{:.7f}".format(min(times))
 
-        metric_table = PrettyTable(['Metric', 'value'])
-        metric_table.add_row(['Max. chunk time', max_chunk_time])
-        metric_table.add_row(['Min. chunk time', min_chunk_time])
-        metric_table.add_row(['Avg. chunk time', average_chunk_time])
-        metric_table.add_row(['Total Transcription time', total_time])
-        print(metric_table)
         # average_time_per_chunk = total_time / rounds
         with open(txt_filename, "r") as f:
             actual_text = f.read()
@@ -70,8 +61,15 @@ async def process_audio_benchmark(chunks_pkl, meta_pkl, txt_filename, size, max_
         print(f"The actual text is:\n{actual_text}")
         print(f'\n\nThe Transcribed text was:\n{finaltext}')
         wer = jiwer.wer(actual_text, transcribed_text, truth_transform=transforms, hypothesis_transform=transforms)
-        print(f"    WER: {wer * 100:.1f}% between {txt_filename} and test")
-        print(f"    Total time using process_audio on test: {total_time} seconds")
+        # print(f"    WER: {wer * 100:.1f}% between {txt_filename} and test")
+        # print(f"    Total time using process_audio on test: {total_time} seconds")
+        metric_table = PrettyTable(['Metric', 'value'])
+        metric_table.add_row(['Max. chunk time', max_chunk_time])
+        metric_table.add_row(['Min. chunk time', min_chunk_time])
+        metric_table.add_row(['Avg. chunk time', average_chunk_time])
+        metric_table.add_row(['Word Error Rate (WER)', f"{wer * 100:.1f}% "])
+        metric_table.add_row(['Total Transcription time', total_time])
+        print(metric_table)
         # print(f"    Average time pr. chunk: {average_time_per_chunk} seconds, chunk size: {chunk_size}")
     except Exception as e:
         print(f"Error creating metrics for file: {txt_filename}: {e}")
