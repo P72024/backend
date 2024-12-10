7cc6a497e08e720777906e6c526720bb6223aa5d
pbaekgaard <peter.baekgaard@outlook.com>
2024-12-10 11:41:47 +0100

    benchmarking ready. just needs files. pushing to test on gpu on server

--- benchmarking/run_benchmarks.py
+++ benchmarking/run_benchmarks.py
@@ -1,51 +1,146 @@
+import argparse
 import asyncio
+import csv
 import os
 import sys
-import yaml
-import csv
+from datetime import datetime
 from itertools import product
 
-from memory_profiler import profile
-from process_audio_benchmark import process_audio_benchmark
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+import yaml
 from ASR.ASR import ASR
+from memory_profiler import profile
+
+from process_audio_benchmark import process_audio_benchmark
 
 
 def get_absolute_path(relative_path):
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), relative_path)
 
+folder_path = 'testfiles/eval_files/'
+results_file_path = get_absolute_path("results/results.csv")
+
+
 # Add the src folder to the Python path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Run Benchmarks with optional GPU usage")
+    
+    # Step 2: Add the --gpu argument with store_true, which defaults to False
+    parser.add_argument('--gpu', action='store_true', help="Use GPU for benchmarking")
+    
+    # Parse arguments
+    return parser.parse_args()
 
 config_file_path = get_absolute_path("config.yaml")
-with open(config_file_path, "r") as f:
-    config = yaml.safe_load(f)
-parameters = config["grid_search"]["parameters"]
-keys, values = zip(*parameters.items())
-combinations = [dict(zip(keys, combo)) for combo in product(*values)]
-total_combinations = len(combinations)
+
+# Final updated combinations
+        
 
 
 
-async def run_benchmarks():
+
+
+
+async def run_benchmarks(use_gpu : bool, combinations, files):
+    total_combinations = len(combinations)
     for i, params in enumerate(combinations, 1):
-        print(f"Combination {i} of {total_combinations}: {params}")
-        #TODO: kør tests på alle parametre fra config og evt flere (lige nu er det kun model_type/size). Test for alle testfilerne. Måske bare smid alle testfiler i samme mappe og så kør testen for alle filer i den mappe.
-        #TODO: EVT. kør mere end een test og så tag et gennemsnit af alle resultaterne.
 
-                                                                    # filnavn tilsvarer paramtetrene fra vadfilteret.
-        results = await process_audio_benchmark(f"{get_absolute_path('testfiles/8-0.8.pkl')}", f"{get_absolute_path('testfiles/benchmark.txt')}", params["model_type"])
+        for file_idx, (filename, file) in enumerate(files, 1):
+            print(f"Combination {i} of {total_combinations} with file {file_idx} of {len(files)}: {params}")
+            #TODO: EVT. kør mere end een test og så tag et gennemsnit af alle resultaterne.
+            transcription_results = await process_audio_benchmark(f"{get_absolute_path(file)}", f"{get_absolute_path('testfiles/benchmark.txt')}", params, use_gpu)
 
-        params = [
-            ['Param', 'Value'],
-            ['Model', params['model_type']],
-        ]
-        results_file_path = get_absolute_path("results/results.csv")
-        #TODO: resultaterne er ret dårlige pt. Tror måske der er noget galt med pkl filerne eller den måde vi læser dem på. Højst sandysnliggt sample rate eller lignende som ikke er blevet opdateret til det nye format fra VADfilteret på fronten.
+            #TODO: skriv tidspunkt(dato), filnavn og parametre plus evalueringskriterier ud på een linje i csv. Og bare tilføj til csv'en i stedet for at overskrive den.
+            csv_row = [
+                datetime.today().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3],
+                filename]
 
-        #TODO: skriv tidspunkt(dato), filnavn og parametre plus evalueringskriterier ud på een linje i csv. Og bare tilføj til csv'en i stedet for at overskrive den.
-        with open(results_file_path, "w") as f:
-            csv.writer(f).writerows(params)
-            csv.writer(f).writerows(results)
+            for (key, val) in params.items():
+                if key == 'confidence_limit' and params["confidence_based"] == False:
+                    csv_row.append("N/A")
+                else:
+                    csv_row.append(val)
+
+            for (_, val) in transcription_results.items():
+                csv_row.append(val)
 
 
-asyncio.run(run_benchmarks())
+            with open(results_file_path, "a", newline='') as f:
+                csv_writer = csv.writer(f)
+                csv_writer.writerow(csv_row)
+
+
+
+async def generate_combinations(config_path):
+    with open(config_path, "r") as f:
+        config = yaml.safe_load(f)
+    parameters = config["grid_search"]["parameters"]["global"]
+    confidence_limits = config["grid_search"]["parameters"]["confidence"]["confidence_limit"]
+    keys, values = zip(*parameters.items())
+    combinations = [dict(zip(keys, combo)) for combo in product(*values)]
+    updated_combinations = []
+    for combination in combinations:
+        if combination["confidence_based"]:
+            # Update the original combination with the first confidence limit
+            combination["confidence_limit"] = confidence_limits[0]
+            updated_combinations.append(combination)
+
+            # Create additional combinations for the other confidence limits
+            for limit in confidence_limits[1:]:
+                new_combination = combination.copy()
+                new_combination["confidence_limit"] = limit
+                updated_combinations.append(new_combination)
+        else:
+            # Keep the combination as is if confidence_based is False
+            combination["confidence_limit"] = 0.0
+            updated_combinations.append(combination)
+    return updated_combinations
+
+
+async def main():
+    args = parse_arguments()
+    titles = [
+            "Date",
+            "Filename",
+                "compute_type",
+            "model_type",
+            "beam_size",
+            "use_context",
+            "confidence_based",
+            "num_workers",
+            "confidence_limit",
+            "Max. chunk time",
+            "Min. chunk time",
+            "Avg. chunk time",
+            "Word Error Rate (WER)",
+            "Word Information Loss (WIL)",
+            "Total Transcription Time",
+            "Total GPU VRAM Usage",
+            "Total GPU Clock Speed",
+            "Peak GPU VRAM Usage",
+            "Peak GPU Clock Speed",
+            "Avg. GPU VRAM Usage",
+            "Avg. GPU Clock Speed",
+            "Total RAM Usage",
+            "Peak RAM Usage",
+            "Avg. RAM Usage",
+            ]
+
+    with open(results_file_path, "w") as f:
+        csv.writer(f).writerow(titles)
+    use_gpu = args.gpu  # True if --gpu is passed, otherwise False
+    combinations = await generate_combinations(config_file_path)
+    print(f"Using GPU: {use_gpu}")
+    files = []
+    for file in os.listdir(folder_path):
+        if file.endswith('.pkl'):
+            files.append((file, folder_path + file))
+    # Example of conditional usage based on use_gpu
+    if use_gpu:
+        print("Running benchmarks on GPU...")
+    else:
+        print("Running benchmarks on CPU...")
+    await run_benchmarks(use_gpu, combinations, files)
+
+asyncio.run(main())
+
--- src/ASR/ASR.py
+++ src/ASR/ASR.py
@@ -1,31 +1,56 @@
-
 import logging
 import re
 from math import ceil
-from typing import List
-import scipy.io.wavfile as wavfile
+
 import numpy as np
 from faster_whisper import WhisperModel
+
 
 class ASR:
     max_context_length = 200
     context:str = ""
-    unfinished_sentence = None
-    previous_transcription = ""
+    prev_chunks = None
+    beam_size = 5
+    use_context = True
+    num_workers = 1
 
-    def __init__ (self, model_size: str, device="auto", compute_type = "float16", max_context_length=200):
-        self.whisper_model = WhisperModel(model_size, device=device, compute_type=compute_type)
+
+    def __init__ (self,
+                  model_size: str,
+                  beam_size: int,
+                  use_context: bool,
+                  num_workers: int,
+                  device="auto",
+                  compute_type = "int8_float32",
+                  max_context_length=200):
         self.max_context_length = max_context_length
+        self.beam_size = beam_size
+        self.use_context = use_context
+        self.num_workers = num_workers
+
+        self.whisper_model = WhisperModel(model_size,
+                                          device=device,
+                                          compute_type=compute_type,
+                                          num_workers=self.num_workers)
         
     def transcribe(self, audio_chunk: np.float32, context: str) -> str:  
         transcribed_text = ""
 
-        segments, info = self.whisper_model.transcribe(
-            audio_chunk, 
-            language='en',
-            beam_size=12,
-            initial_prompt=context,
-        )
+        if self.use_context:
+            segments, _ = self.whisper_model.transcribe(
+                audio_chunk, 
+                append_punctuations=".,?!",
+                language='en',
+                beam_size=self.beam_size,
+                initial_prompt=context,
+            )
+        else:
+            segments, _ = self.whisper_model.transcribe(
+                audio_chunk, 
+                append_punctuations=".,?!",
+                language='en',
+                beam_size=self.beam_size,
+            )
         
         for segment in segments:
             transcribed_text += " " + segment.text
@@ -35,57 +60,28 @@ class ASR:
     
     def process_audio(self, audio_chunk: np.float32) -> str:
         logging.info("[ASR] Processing audio chunk")
+        if self.prev_chunks is not None:
+            audio_chunk = np.concatenate((self.prev_chunks, audio_chunk), axis=0)
+            self.prev_chunks = None
+
         transcribed_text = self.transcribe(audio_chunk, self.context)
-        if "..." in transcribed_text or '- ' in transcribed_text:
-            print('something is unfinished')
-            self.unfinished_sentence = transcribed_text.replace("...", "")  # Remove trailing ellipsis
-            self.unfinished_sentence = transcribed_text.replace("- ", "")
+        if ("..." in transcribed_text or transcribed_text.endswith('-') or transcribed_text.endswith('- ')):
+            print(f"Something is unfinished")
+            self.prev_chunks = audio_chunk
             return ""
         
         # If there was an unfinished sentence, merge it with the new transcription
-        if self.unfinished_sentence:
-            transcribed_text = transcribed_text
-            self.unfinished_sentence = None  # Reset unfinished sentence
         transcribed_text = transcribed_text.lstrip()
         pattern = r'[^a-zA-Z0-9.,\-/?! ]'
         transcribed_text = re.sub(pattern, '', transcribed_text)
-        confirmed_text = self.confirm_text(transcribed_text)
-        print(f"[CONFIRMED TRANSCRIPTION] {confirmed_text}")
 
-        self.update_context(transcribed_text)
+        print(f"[TRANSCRIPTION] {transcribed_text}")
+
+        if self.use_context:
+            self.update_context(transcribed_text)
         logging.info(f"[ASR] Updated context: {self.context}")
     
         return transcribed_text
-
-    def confirm_text(self, transcribed_text: str) -> str:
-        # Split the current and previous transcription into words
-        new_words = transcribed_text.split()
-        prev_words = self.previous_transcription.split()
-        if len(prev_words) == 0:
-            self.previous_transcription = ' '.join(new_words)
-            return ' '.join(new_words)
-        # Initialize a list to store matching words
-        matching_words = []
-
-        # Compare words until two consecutive words differ
-        differences = 0
-        for i, word in enumerate(new_words):
-            if i < len(prev_words) and word == prev_words[i]:
-                matching_words.append(word)
-            else:
-                differences += 1
-                if differences >= 2:
-                    break
-                matching_words.append(word)
-
-        # Update previous transcription for future comparisons
-        if self.previous_transcription == "":
-            self.previous_transcription = transcribed_text
-        else:
-            self.previous_transcription += " " + transcribed_text
-
-        # Join and return the matching prefix as a single string
-        return ' '.join(matching_words)    
 
     def update_context(self, new_text: str):
         """Update context with a sliding window to maintain continuity up to max_context_length words."""
@@ -99,48 +95,3 @@ class ASR:
                 self.context = new_text
             else:
                 self.context += " " + new_text
-        
-
-    def merge_sentences(self, unfinished: str, completed: str) -> str:
-        """Merge unfinished and completed transcriptions by removing overlapping words and handling capitalization."""
-        
-        # Normalize both sentences to lowercase for comparison
-        unfinished_lower = unfinished.strip().lower()
-        completed_lower = completed.strip().lower()
-
-        # Check if both sentences are the same when case is ignored
-        if unfinished_lower == completed_lower:
-            return completed.strip()  # Return the completed sentence only
-
-        # Find the longest overlap from the end of 'unfinished' to the start of 'completed'
-        overlap = self.longest_common_suffix_prefix(unfinished, completed)
-        
-        # Combine the two sentences without duplicating the overlapping part
-        merged_sentence = unfinished + " " + completed[len(overlap):].strip()
-        return merged_sentence.strip()
-
-        
-    def longest_common_suffix_prefix(self, str1: str, str2: str) -> str:
-        """Find the longest common suffix of str1 that matches the prefix of str2."""
-        
-        min_len = min(len(str1), len(str2))
-        for i in range(min_len, 0, -1):
-            if str1[-i:] == str2[:i]:
-                return str1[-i:]
-        return ""
-
-
-    def save_audio_to_file(self, audio_array, sample_rate, filename):
-        # Ensure the array is float32
-        audio_array = audio_array.astype(np.float32)
-        
-        # Normalize to prevent clipping (optional)
-        max_val = np.max(np.abs(audio_array))
-        if max_val > 0:
-            audio_array = audio_array / max_val
-        
-        # Scale to int16 range for WAV file writing
-        scaled_audio = np.int16(audio_array * 32767)
-        
-        # Write to file
-        wavfile.write(filename, sample_rate, scaled_audio)
