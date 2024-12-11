e04d07f25d41436b0c2bf43a4143fe687f283e02
Xaniah <54134770+Xaniah@users.noreply.github.com>
2024-12-11 09:16:31 +0100

    Update ASR.py

--- src/ASR/ASR.py
+++ src/ASR/ASR.py
@@ -7,7 +7,7 @@ import scipy.io.wavfile as wavfile
 import numpy as np
 from faster_whisper import WhisperModel
 
-from .Util import unix_seconds_to_ms
+from ..Util import unix_seconds_to_ms
 
 class ASR:
     max_context_length = 200
