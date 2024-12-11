5ff06385c0903014d30945444e802f50b7eb4f58
Rasmus <rasmu@LAPTOP-08K6AG11>
2024-12-11 14:43:20 +0100

    Beginning touches to stress testing

--- benchmarking/process_audio_benchmark.py
+++ benchmarking/process_audio_benchmark.py
@@ -1,11 +1,15 @@
 import csv
+from multiprocessing import process
 import os
 import pickle
 import subprocess
 import sys
 import time
 from io import BytesIO
+import random
 
+import threading
+from typing import List
 import GPUtil
 import jiwer
 import numpy as np
@@ -42,7 +46,7 @@ def measure_usage():
     return ram_used_gb, gpu_vram_used_mb, gpu_mhz
 
 async def process_audio_benchmark(chunks_pkl, txt_filename, params : dict, use_gpu : bool):
-# Define required keys and their expected types
+    # Define required keys and their expected types
     required_keys_with_types = {
         "model_type": str,
         "beam_size": int,
@@ -64,19 +68,19 @@ async def process_audio_benchmark(chunks_pkl, txt_filename, params : dict, use_g
 
     if params["confidence_based"] == True:
         asr = ASR_tweaked(model_size=params["model_type"],
-                          beam_size=params["beam_size"],
-                          use_context=params["use_context"],
-                          confidence_limit=params["confidence_limit"],
-                          num_workers=params["num_workers"],
-                          device="cuda" if use_gpu else "auto",
-                          compute_type="auto")
+                        beam_size=params["beam_size"],
+                        use_context=params["use_context"],
+                        confidence_limit=params["confidence_limit"],
+                        num_workers=params["num_workers"],
+                        device="cuda" if use_gpu else "auto",
+                        compute_type="auto")
     else:
         asr = ASR(model_size=params["model_type"],
-                  beam_size=params["beam_size"],
-                  use_context=params["use_context"],
-                  num_workers=params["num_workers"],
-                  device="cuda" if use_gpu else "auto",
-                  compute_type="auto")
+                beam_size=params["beam_size"],
+                use_context=params["use_context"],
+                num_workers=params["num_workers"],
+                device="cuda" if use_gpu else "auto",
+                compute_type="auto")
 
     transcribed_text = ""
     actual_text = ""
@@ -104,11 +108,11 @@ async def process_audio_benchmark(chunks_pkl, txt_filename, params : dict, use_g
 
     for (chunk, isLastOfSpeech) in chunks:
         start_total_time = time.time()
-        newText = asr.process_audio(chunk)
-        if newText is not None:
-            while newText.endswith('…'):
-                newText = newText[:-1]
-            transcribed_text += " " +  newText
+        new_text = asr.process_audio(chunk)
+        if new_text is not None:
+            while new_text.endswith('…'):
+                new_text = new_text[:-1]
+            transcribed_text += " " +  new_text
         times.append(time.time() - start_total_time)
         ram_usage_chunk, gpu_usage_vram_chunk, gpu_usage_clock_chunk = measure_usage()
         #VRAM
@@ -195,5 +199,182 @@ async def process_audio_benchmark(chunks_pkl, txt_filename, params : dict, use_g
         "Peak RAM Usage": peak_RAM_usage,
         "Avg. RAM Usage": avg_RAM_usage
     }
+
+    return results
+
+def simulate_client(chunk, client_id, result_dict, asr_model):
+    new_text = asr_model.process_audio(chunk)
+    if new_text is not None:
+        while new_text.endswith('…'):
+            new_text = new_text[:-1]
+        result_dict[client_id] += " " +  new_text
+
+async def process_audio_stress_test(chunks_pkl, txt_filename, params: dict, use_gpu: bool):
+    # Define required keys and their expected types
+    required_keys_with_types = {
+        "num_workers": int,
+        "num_concurrent_clients": int,
+        "model_type": str,
+        "beam_size": int,
+        "use_context": bool,
+        "confidence_limit": float,
+        "confidence_based": bool,
+    }
+
+    # Validate keys and their types
+    missing_keys = required_keys_with_types.keys() - params.keys()
+    assert not missing_keys, f"Missing keys: {missing_keys}"
+
+    for key, expected_type in required_keys_with_types.items():
+        assert isinstance(params[key], expected_type), (
+            f"Key '{key}' must be of type {expected_type.__name__}, "
+            f"but got {type(params[key]).__name__}"
+        )
+
+    if params["confidence_based"] == True:
+        asr = ASR_tweaked(model_size=params["model_type"],
+                        beam_size=params["beam_size"],
+                        use_context=params["use_context"],
+                        confidence_limit=params["confidence_limit"],
+                        num_workers=params["num_workers"],
+                        device="cuda" if use_gpu else "auto",
+                        compute_type="auto")
+    else:
+        asr = ASR(model_size=params["model_type"],
+                beam_size=params["beam_size"],
+                use_context=params["use_context"],
+                num_workers=params["num_workers"],
+                device="cuda" if use_gpu else "auto",
+                compute_type="auto")
+
+    result_dict = dict()
+    for client_id in range(0, params["num_concurrent_clients"]):
+        result_dict[client_id] = ""
+
+    actual_text = ""
+    with open(chunks_pkl, 'rb') as f:
+        chunks = pickle.load(f)
+    # print(f"Number of chunks: {len(chunks)}")
+    # sf.write("benchmark.wav", chunks, samplerate=sr)
+    #Start timer
+    times = []
+    
+    ## GPU VALUES
+    total_GPU_VRAM_usage = 0
+    peak_GPU_VRAM_usage = 0
+    avg_GPU_VRAM_usage = 0
+
+    total_GPU_clock_usage = 0
+    peak_GPU_clock_usage = 0
+    avg_GPU_clock_usage = 0
+
+
+    ## RAM VALUES
+    total_RAM_usage = 0
+    peak_RAM_usage = 0
+    avg_RAM_usage = 0
+
+    # Change it so that simulated clients don't play the same audio.
+    for (chunk, isLastOfSpeech) in chunks:
+        start_total_time = time.time()
+        client_threads: List[threading.Thread] = []
+        for client_id in range(0, params["num_concurrent_clients"]):  # Simulating clients
+            client_thread = threading.Thread(
+                target=simulate_client, args=(chunk, client_id, result_dict, asr)
+            )
+            client_threads.append(client_thread)
+            client_thread.start()
+        
+        for client_thread in client_threads:
+            client_thread.join()
+        times.append(time.time() - start_total_time) # Are we interested in timing each client or total? The same for GPU and RAM usage
+        ram_usage_chunk, gpu_usage_vram_chunk, gpu_usage_clock_chunk = measure_usage()
+        #VRAM
+        total_GPU_VRAM_usage += gpu_usage_vram_chunk
+        if gpu_usage_vram_chunk > peak_GPU_VRAM_usage:
+            peak_GPU_VRAM_usage = gpu_usage_vram_chunk
+
+        #CLOCKSPEED
+        total_GPU_clock_usage += gpu_usage_clock_chunk
+        if gpu_usage_clock_chunk > peak_GPU_clock_usage:
+            peak_GPU_clock_usage = gpu_usage_clock_chunk
+
+        #RAM
+        total_RAM_usage += ram_usage_chunk
+        if ram_usage_chunk > peak_RAM_usage:
+            peak_RAM_usage = ram_usage_chunk
+
+    avg_GPU_VRAM_usage= total_GPU_VRAM_usage / len(chunks)
+    avg_GPU_clock_usage = total_GPU_clock_usage / len(chunks)
+    avg_RAM_usage = total_RAM_usage / len(chunks)
+    #End timer
+    #total time elapsed
+    total_time = sum(times)
+    average_chunk_time = "{:.7f}".format(np.average(times))
+    max_chunk_time = "{:.7f}".format(max(times))
+    min_chunk_time = "{:.7f}".format(min(times))
+
+    # average_time_per_chunk = total_time / rounds
+    with open(txt_filename, "r") as f:
+        actual_text = f.read()
+    # print(actual_text)
+
+            # jiwer.ExpandCommonEnglishContractions(),
+            # jiwer.RemoveEmptyStrings(),
+            # jiwer.ToLowerCase(),
+            # jiwer.RemoveMultipleSpaces(),
+            # jiwer.Strip(),
+            # jiwer.RemovePunctuation(),
+            # jiwer.ReduceToListOfListOfWords(),
+    transforms : jiwer.Compose = jiwer.Compose(
+        [
+            jiwer.ToLowerCase(),
+            jiwer.RemoveEmptyStrings(),
+            jiwer.RemoveMultipleSpaces(),
+            jiwer.RemovePunctuation(),
+            jiwer.ReduceToListOfListOfWords(),
+        ]
+    )
+
+    # print(transforms(transcribed_text))
+    measures = dict()
+    results = dict()
+    for client_id in range(0, params["num_concurrent_clients"]):
+        finaltext = " ".join(transforms(result_dict[client_id])[0])
+        transformed_actual_text = " ".join(transforms(actual_text)[0])
+        # print(f"The actual text is:\n{transformed_actual_text}")
+        # print(f'\n\nThe Transcribed text was:\n{finaltext}')
+        # wer = jiwer.wer(transformed_actual_text, transcribed_text, truth_transform=transforms, hypothesis_transform=transforms, )
+
+        measures[client_id] = jiwer.compute_measures(transformed_actual_text, finaltext, truth_transform=transforms, hypothesis_transform=transforms)
+        # print(f"Measurements from Jiwer: \n{measures}")
+        # print(f"    WER: {wer * 100:.1f}% between {txt_filename} and test")
+        # print(f"    Total time using process_audio on test: {total_time} seconds")
+        # metric_table = PrettyTable(['Metric', 'value'])
+        # metric_table.add_row(['Max. chunk time', max_chunk_time])
+        # metric_table.add_row(['Min. chunk time', min_chunk_time])
+        # metric_table.add_row(['Avg. chunk time', average_chunk_time])
+        # metric_table.add_row(['Word Error Rate (WER)', f"{measures['wer'] * 100:.1f}% "])
+        # metric_table.add_row(['Word Information Loss (WIL)', f"{measures['wil'] * 100:.1f}%"])
+        # metric_table.add_row(['Total Transcription time', total_time])
+        # print(metric_table)
+        # print(f"    Average time pr. chunk: {average_time_per_chunk} seconds, chunk size: {chunk_size}")
+        results[client_id] = {
+            "Max. chunk time": max_chunk_time,
+            "Min. chunk time": min_chunk_time,
+            "Avg. chunk time": average_chunk_time,
+            "Word Error Rate (WER)": f"{measures[client_id]['wer'] * 100:.1f}%",
+            "Word Information Loss (WIL)": f"{measures[client_id]['wil'] * 100:.1f}%",
+            "Total Transcription time": total_time,
+            "Total GPU VRAM Usage": total_GPU_VRAM_usage,
+            "Total GPU Clock Speed": total_GPU_clock_usage,
+            "Peak GPU VRAM Usage": peak_GPU_VRAM_usage,
+            "Peak GPU Clock Speed": peak_GPU_clock_usage,
+            "Avg. GPU VRAM Usage": avg_GPU_VRAM_usage,
+            "Avg. GPU Clock Speed": avg_GPU_clock_usage,
+            "Total RAM Usage": total_RAM_usage,
+            "Peak RAM Usage": peak_RAM_usage,
+            "Avg. RAM Usage": avg_RAM_usage
+        }
 
     return results
--- benchmarking/run_benchmarks.py
+++ benchmarking/run_benchmarks.py
@@ -5,6 +5,8 @@ import os
 import sys
 from datetime import datetime
 from itertools import product
+from pydub import AudioSegment
+from pydub.playback import play
 
 from rich.progress import (BarColumn, Progress, TaskProgressColumn, TextColumn,
                            TimeRemainingColumn)
@@ -12,10 +14,9 @@ from rich.progress import (BarColumn, Progress, TaskProgressColumn, TextColumn,
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 import yaml
 from memory_profiler import profile
-from process_audio_benchmark import process_audio_benchmark
+from process_audio_benchmark import process_audio_benchmark, process_audio_stress_test
 
 from ASR.ASR import ASR
-
 
 def get_absolute_path(relative_path):
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), relative_path)
@@ -23,6 +24,8 @@ def get_absolute_path(relative_path):
 folder_avg_path = 'testfiles/eval_files/'
 results_file_path = get_absolute_path("results/results.csv")
 results_avg_file_path = get_absolute_path("results/results_avg.csv")
+stress_test_results_file_path = get_absolute_path("results/stress_test_results.csv")
+stress_test_results_avg_file_path = get_absolute_path("results/stress_test_results_avg.csv")
 
 
 # Add the src folder_avg to the Python path
@@ -31,6 +34,8 @@ def parse_arguments():
     
     # Step 2: Add the --gpu argument with store_true, which defaults to False
     parser.add_argument('--gpu', action='store_true', help="Use GPU for benchmarking")
+    parser.add_argument('--stress', action='store_true', help="Run stress test")
+    parser.add_argument('--benchmarking', action='store_true', help="Run benchmarking")
     
     # Parse arguments
     return parser.parse_args()
@@ -40,10 +45,6 @@ config_file_path = get_absolute_path("config.yaml")
 # Final updated combinations
         
 num_iterations = 3
-
-
-
-
 
 
 async def run_benchmarks(use_gpu : bool, combinations, files):
@@ -67,7 +68,6 @@ async def run_benchmarks(use_gpu : bool, combinations, files):
             for i, params in enumerate(combinations, 1):
                 progress.reset(iterationProgress)
                 progress.console.print(f"Combination {i} of {total_combinations} with file {file_idx} of {total_files}: {params}")
-                #TODO: EVT. kør mere end een test og så tag et gennemsnit af alle resultaterne.
 
                 results_array = []
                 for i in range(num_iterations):
@@ -179,34 +179,32 @@ async def generate_combinations(config_path):
             updated_combinations.append(combination)
     return updated_combinations
 
-
-async def main():
-    args = parse_arguments()
+async def run_benchmarking(args):
     titles = [
-            "Date",
-            "Filename",
-            "model_type",
-            "beam_size",
-            "use_context",
-            "confidence_based",
-            "num_workers",
-            "confidence_limit",
-            "Max. chunk time",
-            "Min. chunk time",
-            "Avg. chunk time",
-            "Word Error Rate (WER)",
-            "Word Information Loss (WIL)",
-            "Total Transcription Time",
-            "Total GPU VRAM Usage",
-            "Total GPU Clock Speed",
-            "Peak GPU VRAM Usage",
-            "Peak GPU Clock Speed",
-            "Avg. GPU VRAM Usage",
-            "Avg. GPU Clock Speed",
-            "Total RAM Usage",
-            "Peak RAM Usage",
-            "Avg. RAM Usage",
-            ]
+                "Date",
+                "Filename",
+                "model_type",
+                "beam_size",
+                "use_context",
+                "confidence_based",
+                "num_workers",
+                "confidence_limit",
+                "Max. chunk time",
+                "Min. chunk time",
+                "Avg. chunk time",
+                "Word Error Rate (WER)",
+                "Word Information Loss (WIL)",
+                "Total Transcription Time",
+                "Total GPU VRAM Usage",
+                "Total GPU Clock Speed",
+                "Peak GPU VRAM Usage",
+                "Peak GPU Clock Speed",
+                "Avg. GPU VRAM Usage",
+                "Avg. GPU Clock Speed",
+                "Total RAM Usage",
+                "Peak RAM Usage",
+                "Avg. RAM Usage",
+                ]
 
     folder_iter = os.path.dirname(results_file_path)
     if folder_iter:  # Only create folder if there's a directory specified
@@ -226,7 +224,7 @@ async def main():
     combinations = await generate_combinations(config_file_path)
     print(f"Using GPU: {use_gpu}")
     files = []
-    for file in os.listdir(folder_avg_path):
+    for file in os.listdir(get_absolute_path(folder_avg_path)):
         if file.endswith('.pkl'):
             files.append((file, folder_avg_path + file))
     # Example of conditional usage based on use_gpu
@@ -235,6 +233,142 @@ async def main():
     else:
         print("Running benchmarks on CPU...")
     await run_benchmarks(use_gpu, combinations, files)
+
+
+
+async def run_stress_test(use_gpu: bool, combinations, files):
+    total_combinations = len(combinations)
+    total_files = len(files)
+    total_progress_length = total_files * total_combinations * num_iterations
+    print(f"Running Stress testing with the following settings:\nNumber of combination: {total_combinations}\nNumber of Iterations pr. Combination: {num_iterations}\nNumber of files: {total_files}\nGPU Enabled: {use_gpu}")
+    with Progress(
+    TextColumn("[progress.description]{task.description}"),
+    BarColumn(),
+    TaskProgressColumn(),
+    TimeRemainingColumn(),
+    ) as progress:
+        iterationProgress = progress.add_task("[yellow]Running Iterations...", total=num_iterations)
+        combinationsProgress = progress.add_task("[blue]Benchmarking Combinations...", total=total_combinations)
+        fileProgress = progress.add_task("[red]Benchmarking files...", total=total_files)
+        totalProgression = progress.add_task('[green]Total Progress...', total=total_progress_length)
+
+
+        for file_idx, (filename, file) in enumerate(files, 1):
+            progress.reset(combinationsProgress)
+            for i, params in enumerate(combinations, 1):
+                progress.reset(iterationProgress)
+                progress.console.print(f"Combination {i} of {total_combinations} with file {file_idx} of {total_files}: {params}")
+
+                results_array = []
+                for i in range(num_iterations):
+                    transcription_results = await process_audio_stress_test(f"{get_absolute_path(file)}", f"{get_absolute_path('testfiles/benchmark.txt')}", params, use_gpu)
+                    results_array.append(transcription_results)
+
+                    csv_row = [
+                        datetime.today().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3],
+                        filename
+                    ]
+
+                    for (key, val) in params.items():
+                        if key == 'confidence_limit' and params["confidence_based"] == False:
+                            csv_row.append("N/A")
+                        else:
+                            csv_row.append(val)
+
+                    for (_, val) in transcription_results.items():
+                        csv_row.append(val)
+                    
+                    with open(stress_test_results_file_path, "a", newline='') as f:
+                        csv_writer = csv.writer(f)
+                        csv_writer.writerow(csv_row)
+                    progress.update(iterationProgress, advance=1)
+                    progress.update(totalProgression, advance=1)
+
+                
+                progress.update(combinationsProgress, advance=1)
+            progress.update(fileProgress, advance=1)
+
+
+def generate_stress_test_combinations(config_path):
+    with open(config_path, "r") as f:
+        config = yaml.safe_load(f)
+    parameters = config["stress_test"]["parameters"]["global"]
+    confidence_limits = config["grid_search"]["parameters"]["confidence"]["confidence_limit"]
+    keys, values = zip(*parameters.items())
+    combinations = [dict(zip(keys, combo)) for combo in product(*values)]
+    updated_combinations = []
+    for combination in combinations:
+        if combination["confidence_based"]:
+            # Update the original combination with the first confidence limit
+            combination["confidence_limit"] = confidence_limits[0]
+            updated_combinations.append(combination)
+
+            # Create additional combinations for the other confidence limits
+            for limit in confidence_limits[1:]:
+                new_combination = combination.copy()
+                new_combination["confidence_limit"] = limit
+                updated_combinations.append(new_combination)
+        else:
+            # Keep the combination as is if confidence_based is False
+            combination["confidence_limit"] = 0.0
+            updated_combinations.append(combination)
+    return updated_combinations
+
+async def run_stress_testing(args):
+    titles = [
+        "Date",
+        "Filename",
+        "num_concurrent_clients",
+        "num_workers",
+        "Word Error Rate (WER)",
+        "Word Information Loss (WIL)",
+        "Total Transcription Time",
+        "Total GPU VRAM Usage",
+        "Total GPU Clock Speed",
+        "Peak GPU VRAM Usage",
+        "Peak GPU Clock Speed",
+        "Avg. GPU VRAM Usage",
+        "Avg. GPU Clock Speed",
+        "Total RAM Usage",
+        "Peak RAM Usage",
+        "Avg. RAM Usage",
+    ]
+
+    folder_iter = os.path.dirname(stress_test_results_file_path)
+    if folder_iter:  # Only create folder if there's a directory specified
+        os.makedirs(folder_iter, exist_ok=True)
+
+    folder_avg = os.path.dirname(stress_test_results_avg_file_path)
+    if folder_avg:  # Only create folder if there's a directory specified
+        os.makedirs(folder_avg, exist_ok=True)
+
+    with open(stress_test_results_avg_file_path, "w") as f:
+        csv.writer(f).writerow(titles)
+
+    with open(stress_test_results_file_path, "w") as f:
+        csv.writer(f).writerow(titles)
+
+    use_gpu = args.gpu  # True if --gpu is passed, otherwise False
+    combinations = generate_stress_test_combinations(config_file_path)
+    print(f"Using GPU: {use_gpu}")
+    files = []
+    for file in os.listdir(get_absolute_path(folder_avg_path)):
+        if file.endswith('.pkl'):
+            files.append((file, folder_avg_path + file))
+    # Example of conditional usage based on use_gpu
+    if use_gpu:
+        print("Running stress tests on GPU...")
+    else:
+        print("Running stress tests on CPU...")
+    await run_stress_test(use_gpu, combinations, files)
+    
+
+async def main():
+    args = parse_arguments()
+    if args.benchmarking:
+        await run_benchmarking(args)
+    if args.stress:
+        await run_stress_testing(args)
 
 asyncio.run(main())
 
