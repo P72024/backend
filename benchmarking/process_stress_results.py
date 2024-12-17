85a3eee9bb9111c4071c7204ec3ff44919608c17
Rasmus <rasmu@LAPTOP-08K6AG11>
2024-12-17 13:46:40 +0100

    Stress testing results ready to be made

--- benchmarking/process_stress_results.py
+++ benchmarking/process_stress_results.py
@@ -5,9 +5,16 @@ import numpy as np
 import ast
 
 def plot_scatter(df, x_col, y_col, xlabel, ylabel, title):
+    coefficients = np.polyfit(df[x_col], df[y_col], 2)
+    a, b, c = coefficients
+    regression_line = a*(df[x_col]**2) + b*(df[x_col]) + c
+
+    y=f'{a:.2f}x^2 + {b:.2f}x + {c:.2f}'
+
     plt.figure(figsize=(10, 5))
     plt.scatter(df[x_col], df[y_col])
-    plt.plot(df[x_col], df[y_col])
+    plt.plot(df[x_col], regression_line, label='Regression Line (y={a:.2f}x^2 + {b:.2f}x + {c:.2f}')
+    plt.text(10, 40.5, y, fontsize=12)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.title(title)
@@ -20,9 +27,11 @@ stress_test_results = pd.read_csv(path_stress_test)
 def run_process_stress_results(results):
     df = pd.DataFrame(results, columns=["num_workers", "num_concurrent_rooms", "client_results"])
     df["client_results"] = df["client_results"].apply(ast.literal_eval)
-    df_specific_num_of_rooms = df[df["num_concurrent_rooms"] == 2]
-    max_queue_time_dict, min_queue_time_dict, avg_queue_time_dict, avg_queue_time_with_rooms_dict = {}, {}, {}, {}
+    df_specific_num_of_rooms = df[df["num_concurrent_rooms"] == 32]
+    df_specific_num_of_workers = df[df["num_workers"] == 16]
+    max_queue_time_dict, min_queue_time_dict, avg_queue_time_dict, avg_queue_time_with_rooms_dict, avg_queue_time_with_workers_dict = {}, {}, {}, {}, {}
 
+    print(df["num_concurrent_rooms"])
     for index, row in df.iterrows():
         max_queue_time_dict[index] = []
         min_queue_time_dict[index] = []
@@ -31,16 +40,23 @@ def run_process_stress_results(results):
             max_queue_time_dict[index].append(float(value["Max. queue time"]) / 1000)
             min_queue_time_dict[index].append(float(value["Min. queue time"]) / 1000)
             avg_queue_time_dict[index].append(float(value["Avg. queue time"]) / 1000)
-    
+
     for index, row in df_specific_num_of_rooms.iterrows():
         avg_queue_time_with_rooms_dict[index] = []
         for client_id, value in row["client_results"].items():
-            avg_queue_time_with_rooms_dict[index].append(float(value["Max. queue time"]) / 1000)
+            avg_queue_time_with_rooms_dict[index].append(float(value["Avg. queue time"]) / 1000)
+
+    for index, row in df_specific_num_of_workers.iterrows():
+        avg_queue_time_with_workers_dict[index] = []
+        for client_id, value in row["client_results"].items():
+            avg_queue_time_with_workers_dict[index].append(float(value["Avg. queue time"]) / 1000)
+
 
     max_queue_time = []
     min_queue_time = []
     avg_queue_time = []
     avg_queue_time_with_rooms = []
+    avg_queue_time_with_workers = []
 
     for index, val in max_queue_time_dict.items():
         max_queue_time.append(sum(val)/ len(val))
@@ -48,8 +64,11 @@ def run_process_stress_results(results):
         min_queue_time.append(sum(val)/ len(val))
     for index, val in avg_queue_time_dict.items():
         avg_queue_time.append(sum(val)/ len(val))
+
     for index, val in avg_queue_time_with_rooms_dict.items():
         avg_queue_time_with_rooms.append(sum(val)/ len(val))
+    for index, val in avg_queue_time_with_workers_dict.items():
+        avg_queue_time_with_workers.append(sum(val)/ len(val))
 
     plot_scatter({ 
             "Avg. queue time": avg_queue_time,
@@ -63,15 +82,15 @@ def run_process_stress_results(results):
     )
 
     # Latency for number of workers with specific number of rooms, so that we can conclude what effect workers have on queue time
-    plot_scatter({ 
-            "Avg. queue time": avg_queue_time_with_rooms,
-            "Number of workers": df_specific_num_of_rooms["num_workers"]
-        }, 
-        "Number of workers", 
-        "Avg. queue time", 
-        "Number of workers",
-        "Avg. queue time", 
-        "Avg. queue time vs. Number of workers"
-    )
+    # plot_scatter({ 
+    #         "Avg. queue time": avg_queue_time_with_rooms,
+    #         "Number of workers": df_specific_num_of_rooms["num_workers"]
+    #     }, 
+    #     "Number of workers", 
+    #     "Avg. queue time", 
+    #     "Number of workers",
+    #     "Avg. queue time", 
+    #     "Avg. queue time vs. Number of workers"
+    # )
 
 run_process_stress_results(stress_test_results)
